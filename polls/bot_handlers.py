1c0b3a818779b7dcfc270305a1703d24989b20bb
Juan Madurga <jlmadurga@gmail.com>
2016-02-09 14:09:01 +0100

    0.5.0 django-telegram-bot

--- polls/bot_handlers.py
+++ polls/bot_handlers.py
@@ -4,7 +4,7 @@ from polls.bot_views import StartCommandView, UnknownCommandView, \
 from telegrambot.handlers import command, unknown_command
 from telegrambot.bot_views.decorators import login_required
 
-bothandlers = [command('start', StartCommandView.as_command_view()),
+urlpatterns = [command('start', StartCommandView.as_command_view()),
                command('question', QuestionCommandView.as_command_view()),
                command('vote', login_required(VoteCommandView.as_command_view())),
                command('help', HelpCommandView.as_command_view()),
